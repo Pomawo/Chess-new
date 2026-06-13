99a06e7310168682c6a17dba2988dcdacf167ff2
Ordep0152 <vitalijcernavka@gmail.com>
2026-06-13 23:31:21 +0300

    Beauty field

--- chess.cpp
+++ chess.cpp
@@ -178,14 +178,28 @@ bool movementCheck() {
 }
 
 void drawField() {
+    for (int i = 0; i < 33; i++) {
+        std::cout << '-';
+    }
+    std::cout << std::endl;
     for (int y = 0; y < 8; y++){
+        std::cout << '|';
         for(int x = 0; x < 8; x++) {
             if (pieces[y][x] == 0) {
-                std::cout << field[y][x] << ' ';
+                if (x == 0) {
+                    std::cout << ' ' << field[y][x] << ' ' << '|';
+                }
+                else {
+                    std::cout << ' ' << field[y][x] << ' ' << '|';
+                }
             }
             else {
-                std::cout << pieces[y][x] << ' ';
+                std::cout << ' ' << pieces[y][x] << ' ' << '|';
             }
+        }
+        std::cout << std::endl;
+        for (int i = 0; i < 33; i++) {
+            std::cout << '-';
         }
         std::cout << std::endl;
     }
